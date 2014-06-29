8898428ce1b2fe19c6f6100111aef0a0135f0e38
seanbrookes <seanbrookes@beachair.ca>
2014-06-28 19:23:58 -0700

    explorer IA
    - continued to refactor and clean up the rendered markup
    for the explorer view.
    - the show hide tree view events are all wired up and working
    - each 'node' on the tree is now a distinct react component

--- client/www/scripts/modules/explorer/explorer.react.js
+++ client/www/scripts/modules/explorer/explorer.react.js
@@ -1,32 +1,37 @@
 /** @jsx React.DOM */
 var ExplorerMain = (ExplorerMain = React).createClass({
+  getInitialState: function() {
+    return {
+      isOpen: false,
+      isParamsOpenState: false
+    };
+  },
   render: function() {
     var scope = this.props.scope;
     var explorerResources = scope.explorerResources;
-    var items = <li></li>;
+    var cx = React.addons.classSet;
+    var that = this;
 
-    items = explorerResources.map(function(resource) {
+    var mainClasses = cx({
+      'explorer-model is-open': this.state.isOpen,
+      'explorer-model is-closed': !this.state.isOpen
+    });
+
+
+
+    var items = explorerResources.map(function(resource) {
       var config = JSON.stringify(resource.config);
 
       var apis = resource.config.apis.map(function(api) {
-      var apiDetails = api.operations[0];
+        var apiDetails = api.operations[0];
 
-        var parameters = apiDetails.parameters.map(function(param) {
-          return (
-            <li>name: <em>{param.name}</em>  [type: {param.paramType}]</li>);
-        });
         return (
           <li className="explorer-api-detail-item">
-            <div className="explorer-endpoint-title">{apiDetails.nickname}</div>
-            <div className="explorer-endpoint-container">
-              <div>path: {api.path}</div>
-              <div>method: {apiDetails.httpMethod}</div>
-              <div className="explorer-parameters-container">
-                <button className="btn-explorer-property-toggle">parameters:</button>
-                <ul className="explorer-parameters-list">{parameters}</ul>
-              </div>
-            </div>
-          </li>);
+            <ExploreModelApiEndPoint apiDetails={apiDetails} api={api} />
+          </li>
+          );
+
+
       });
       var getNameFromPath = function(resource) {
         var rawPath = resource.path;
@@ -35,11 +40,15 @@ var ExplorerMain = (ExplorerMain = React).createClass({
 
         return returnPath;
       };
+
+      var explorerMainModelClicked = function(event) {
+        var isOpenState = !that.state.isOpen;
+        that.setState({isOpen:isOpenState});
+      };
       return (<li>
-        <button className="btn btn-default btn-block">{getNameFromPath(resource)}</button>
-        <div>
-        <h3>Model API End Points</h3>
-        <ul>{apis}</ul>
+        <button onClick={explorerMainModelClicked} className="btn btn-default btn-block btn-explorer-model-main">{getNameFromPath(resource)}</button>
+        <div className={mainClasses}>
+          <ul>{apis}</ul>
         </div>
       </li>);
     });
@@ -51,10 +60,87 @@ var ExplorerMain = (ExplorerMain = React).createClass({
             <span className="title">explorer</span>
           </div>
           <div className="explorer-view-body">
-            Explorer Goes Here
+            API Explorer
             <ul className="explorer-model-list">{items}</ul>
           </div>
         </div>
+      );
+  }
+});
+var ExploreModelApiEndPoint = (ExploreModelApiEndPoint = React).createClass({
+  getInitialState: function() {
+    return {
+      isApiOpenState: false
+    };
+  },
+  render: function() {
+    var that = this;
+    var apiDetails = that.props.apiDetails;
+    var api = that.props.api;
+    var cx = React.addons.classSet;
+    var apiClasses = cx({
+      'explorer-model-api-details is-open': that.state.isApiOpenState,
+      'explorer-model-api-details is-closed': !that.state.isApiOpenState
+    });
+    var explorerModelApiClicked = function(event) {
+      var isApiOpenState = !that.state.isApiOpenState;
+      that.setState({isApiOpenState:isApiOpenState})
+    };
+    return (
+      <div>
+        <button onClick={explorerModelApiClicked} className="btn btn-block explorer-endpoint-title">{apiDetails.nickname}</button>
+        <div className={apiClasses}>
+          <div>path: {api.path}</div>
+          <div>method: {apiDetails.httpMethod}</div>
+          <div>response class: {apiDetails.responseClass}</div>
+          <div>summary: {apiDetails.summary}</div>
+          <div>errorResponses: {apiDetails.errorResponses}</div>
+
+
+          <ExplorerModelApiParameters parameters={apiDetails.parameters} />
+        </div>
+      </div>);
+  }
+});
+var ExplorerModelApiParameters = (ExplorerModelApiParameters = React).createClass({
+  getInitialState: function() {
+    return {
+      isParamsOpenState: false
+    };
+  },
+  render: function() {
+    var cx = React.addons.classSet;
+    var that = this;
+    var parametersClasses = cx({
+      'explorer-model-api-parameters is-open': this.state.isParamsOpenState,
+      'explorer-model-api-parameters is-closed': !this.state.isParamsOpenState
+    });
+
+    var parameters = this.props.parameters.map(function(param) {
+      return (
+        <li>
+          <ul className="explorer-parameter-item-parameter-list">
+            <li>name: <em>{param.name}</em></li>
+            <li>paramType: <em>{param.paramType}</em></li>
+            <li>description: <em>{param.description}</em></li>
+            <li>dataType: <em>{param.dataType}</em></li>
+            <li>required: <em>{param.required}</em></li>
+            <li>allowMultiple: <em>{param.allowMultiple}</em></li>
+          </ul>
+        </li>
+        );
+    });
+
+    var explorerModelApiParametersClicked = function(event) {
+      var isParamsOpenState = !that.state.isParamsOpenState;
+      that.setState({isParamsOpenState:isParamsOpenState});
+    };
+
+    return (
+      <div className="explorer-parameters-container">
+        <button onClick={explorerModelApiParametersClicked} className="btn-explorer-property-toggle">parameters:</button>
+        <ul className={parametersClasses}>{parameters}</ul>
+      </div>
       );
   }
 });
