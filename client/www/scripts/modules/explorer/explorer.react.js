5991110f05f573fa14fc674f182cd3727b632f2b
seanbrookes <seanbrookes@beachair.ca>
2014-07-03 08:50:29 -0700

    map all the models to the memor db
    - simplify things for the demo

--- client/www/scripts/modules/explorer/explorer.react.js
+++ client/www/scripts/modules/explorer/explorer.react.js
@@ -307,7 +307,7 @@ var ExploreModelApiEndPoint = (ExploreModelApiEndPoint = React).createClass({
                 </form>
               </div>
               <div data-ui-type="cell">
-                <div className="explorer-api-textarea">{JSON.stringify(scope.currentExplorerApiResponse)}</div>
+                <textarea className="explorer-api-textarea" value={JSON.stringify(scope.currentExplorerApiResponse)}></textarea>
               </div>
             </div>
           </div>
