fbdcda58207b8e200540aba4715db392be9bfc3f
Ritchie Martori <skawful@gmail.com>
2014-08-13 16:26:11 -0700

    Remove dependency on loopback

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
-var loopback = require('loopback');
+var express = require('express');
 var path = require('path');
-var app = module.exports = loopback();
+var app = module.exports = express();
 var workspace = require('loopback-workspace');
 
-// boot the loopback app
-app.boot(__dirname);
-
-// basic middleware
-app.use(loopback.favicon());
-app.use(loopback.logger(app.get('env') === 'development' ? 'dev' : 'default'));
-app.use(loopback.cookieParser(app.get('cookieSecret')));
-app.use(loopback.token({model: app.models.accessToken}));
-
 // REST APIs
-app.use(app.get('restApiRoot'), loopback.rest());
 app.use('/workspace', workspace);
 
-// express router
-app.use(app.router);
-
 // static files
-app.use(loopback.static(path.join(__dirname, 'client/www')));
-
-// error handlers
-app.use(loopback.urlNotFound());
-app.use(loopback.errorHandler());
+app.use(express.static(path.join(__dirname, 'client/www')));
