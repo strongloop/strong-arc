02453f8bdeded42dda37a7e531f2c5f2d5476ab4
Miroslav Bajtoš <miroslav@strongloop.com>
2014-08-15 10:50:37 +0200

    Add jshint for test/ files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 var less = require('gulp-less');
 var mocha = require('gulp-spawn-mocha');
+var runSequence = require('run-sequence');
 
-gulp.task('default', ['build-less', 'test']);
+var SOURCES = {
+  BACKEND_TESTS: 'test/*.js'
+};
+
+gulp.task('default', ['build', 'test']);
+
+gulp.task('build', ['build-less']);
 
 gulp.task('build-less', function(){
   return gulp.src('client/less/style.less')
@@ -10,9 +18,24 @@ gulp.task('build-less', function(){
     .pipe(gulp.dest('client/www/style'));
 });
 
-gulp.task('test', ['test-backend']);
+gulp.task('test', ['build'], function(callback) {
+  runSequence(
+    'jshint',
+    'test-backend',
+     callback);
+});
+
+gulp.task('jshint', function() {
+  return gulp.src([
+    SOURCES.BACKEND_TESTS,
+    // TODO(bajtos) add more files once they pass the linter
+  ])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
+});
 
 gulp.task('test-backend', function() {
-  return gulp.src(['test/*.js'], { read: false })
+  return gulp.src([SOURCES.BACKEND_TESTS], { read: false })
     .pipe(mocha());
 });
