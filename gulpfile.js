05bdac6b3b01dfbb88e83685356a7b32ca967a13
Miroslav Bajtoš <miroslav@strongloop.com>
2014-08-15 10:33:09 +0200

    Add backend mocha tests to `test/`
    Add `gulp test` task to run all tests, `gulp test-backend` to run mocha
    tests of backend.
    
    Configure `npm test` to run `gulp test`.
    
    Add loopback-workspace as a package dependency.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var mocha = require('gulp-spawn-mocha');
+
+gulp.task('default', ['build-less', 'test']);
+
 gulp.task('build-less', function(){
   return gulp.src('client/less/style.less')
     .pipe(less())
     .pipe(gulp.dest('client/www/style'));
+});
+
+gulp.task('test', ['test-backend']);
+
+gulp.task('test-backend', function() {
+  return gulp.src(['test/*.js'], { read: false })
+    .pipe(mocha());
 });
---
+++ test/smoke.js
@@ -0,0 +1,12 @@
+var expect = require('chai').expect;
+var request = require('supertest');
+var app = require('../app.js');
+
+describe('Studio', function() {
+  it('serves an HTML page on "/" URL', function(done) {
+    request(app).get('/')
+      .expect(200)
+      .expect('Content-Type', /html/)
+      .end(done);
+  });
+});
