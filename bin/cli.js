6b7f05e0897bc5db0fa6ad9cddebb780542c6cbb
Ritchie Martori <skawful@gmail.com>
2014-08-18 14:13:36 -0700

    Add basic cli `strong-studio`

---
+++ bin/cli.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+var path = require('path');
+var studio = require('../server/server');
+var DEFAULT_STUDIO_HOST = 'localhost';
+var STUDIO_RUNNING_MSG = 
+exports.STUDIO_RUNNING_MSG = 'Your studio is running here:';
+var argv = getArgv();
+var pathArg = argv[0];
+var WORKSPACE_DIR = process.cwd();
+
+if(pathArg) {
+  WORKSPACE_DIR = path.join(WORKSPACE_DIR, pathArg);
+}
+
+process.env.WORKSPACE_DIR = process.env.WORKSPACE_DIR || WORKSPACE_DIR;
+
+var server = studio.listen(0, function(err) {
+  if(err) {
+    console.error('could not start studio!');
+    console.error(err);
+  }
+
+  console.log('%s http://%s:%s',
+    STUDIO_RUNNING_MSG, DEFAULT_STUDIO_HOST,
+    server.address().port);
+});
+
+function getArgv() {
+  var argv = process.argv.splice(); // copy
+  argv.shift(); // remove command
+  return argv;
+}
