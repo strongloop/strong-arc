dc1dc7dfc3459706f1f05777c0795342eaff66a2
Miroslav Bajtoš <miroslav@strongloop.com>
2014-08-20 16:04:30 +0200

    server: add API explorer
    The explorer provided by loopback-workspace does not work when the
    workspace app is mounted at a sub-path. The solution is to mount
    a new explorer instance configured with the correct API path.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var path = require('path');
-var app = module.exports = express();
 var workspace = require('loopback-workspace');
+var explorer = require('loopback-explorer');
+
+var app = module.exports = express();
 
 // REST APIs
 app.use('/workspace', workspace);
+
+// API explorer
+app.use('/explorer', explorer(workspace, { basePath: '/workspace/api' }));
 
 // static files
 app.use(express.static(path.join(__dirname, '../client/www')));
