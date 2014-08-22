7fcc2b3943f025cc1abdfc9a3a54d255ce063bbc
seanbrookes <seanbrookes@beachair.ca>
2014-08-22 12:21:47 -0700

    first cut at moving datasource visual design over
    - there are still some ragged edges that I think should
    go in a separate PR - general vd cleanup as they are not
    necessarily related to datasource design merge

--- client/www/scripts/modules/datasource/datasource.react.js
+++ client/www/scripts/modules/datasource/datasource.react.js
@@ -59,14 +59,13 @@ var DatasourceEditorView = (DatasourceEditorView = React).createClass({
     };
 
     return (
-      <div data-id="DatasourceEditorInstanceContainer" >
+      <div data-id="DatasourceEditorInstanceContainer" className="datasource-editor-instance-container">
         <form name="DatasourceForm" role="form">
-          <button onClick={testConnection}>Test Connection</button>
-          <input type="hidden" data-name="id" name="id" value={dsModel.id} />
-          <div className="form-group">
-            <label for="name">Name</label>
-            <input id="name"
-              className="form-control"
+          <div className="model-header-container">
+            <div className="model-header-name-container form-group">
+              <label for="name">Name</label>
+              <input id="name"
+              className="model-instance-name form-control"
               name="name"
               type="text"
               value={dsModel.name}
@@ -74,90 +73,117 @@ var DatasourceEditorView = (DatasourceEditorView = React).createClass({
               data-name="name"
               placeholder="name"
               required="true" />
+            </div>
+            <button type="button" onClick={saveHandler} className="model-detail-pocket-button model-save-button">Save Datasource</button>
           </div>
-          <div className="form-group">
-            <label for="connector">Connector</label>
-            <select
-              id="connector"
-              value={dsModel.connector}
-              onChange={this.handleChange}
-              data-name="connector"
-              name="connector" >
-              <option value="">choose</option>
-              <option value="memory">In-Memory</option>
-              <option value="oracle">Oracle</option>
-              <option value="mssql">MS SQL</option>
-              <option value="mysql">MySQL</option>
-              <option value="postgresql">PostgreSQL</option>
-              <option value="mongodb">MongoDB</option>
-            </select>
+          <div className="lineBreak"></div>
+          <div className="datasource-field-container">
+            <div data-ui-type="table"  className="datasource-layout-table">
+              <div data-ui-type="row" className="datasource-layout-row">
+                <div data-ui-type="cell" className="datasource-layout-col1">
+                  <div className="username-input-container">
+                    <div className="datasource-form-group">
+                      <label for="userName">Username</label>
+                      <input id="userName"
+                      className="form-control"
+                      name="userName"
+                      type="text"
+                      value={dsModel.userName}
+                      onChange={this.handleChange}
+                      data-name="userName"
+                      placeholder="username" />
+                    </div>
+                  </div>
+                </div>
+                <div data-ui-type="cell" className="datasource-layout-col2">
+                  <div className="password-container">
+                    <div className="datasource-form-group">
+                      <label for="password">Password</label>
+                      <input id="password"
+                      className="form-control"
+                      name="password"
+                      value={dsModel.password}
+                      onChange={this.handleChange}
+                      data-name="password"
+                      type="password"
+                      placeholder="password" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="lineBreak"></div>
+            <div data-ui-type="table" className="datasource-layout-table">
+              <div data-ui-type="row" className="datasource-layout-row">
+                <div data-ui-type="cell" className="datasource-layout-col1">
+                  <div className="datasource-form-group">
+                    <label for="host">Host</label>
+                    <input id="host"
+                    className="form-control"
+                    name="host"
+                    value={dsModel.host}
+                    onChange={this.handleChange}
+                    data-name="host"
+                    type="text"
+                    placeholder="host" />
+                  </div>
+                </div>
+                <div data-ui-type="cell" className="datasource-layout-col2">
+                  <div className="datasource-form-group">
+                    <label for="port">Port</label>
+                    <input id="port"
+                    className="form-control"
+                    name="port"
+                    value={dsModel.port}
+                    onChange={this.handleChange}
+                    data-name="port"
+                    type="text"
+                    placeholder="port" />
+                  </div>
+                </div>
+              </div>
+              <div data-ui-type="row"  className="datasource-layout-row">
+                <div data-ui-type="cell" className="datasource-layout-col1">
+                  <input type="hidden" data-name="id" name="id" value={dsModel.id} />
+                  <div className="datasource-form-group">
+                    <label for="database">Database</label>
+                    <input id="database"
+                    className="form-control"
+                    name="database"
+                    value={dsModel.database}
+                    onChange={this.handleChange}
+                    data-name="database"
+                    type="text"
+                    placeholder="database name" />
+                  </div>
+                </div>
+                <div data-ui-type="cell" className="datasource-layout-col2">
+                  <div className="form-group">
+                    <label for="connector">Connector</label>
+                    <select className=
+                    "model-instance-editor-input"
+                    id="connector"
+                    value={dsModel.connector}
+                    onChange={this.handleChange}
+                    data-name="connector"
+                    name="connector" >
+                      <option value="">choose</option>
+                      <option value="loopback-connector-oracle">Oracle</option>
+                      <option value="loopback-connector-mssql">MsSQL</option>
+                      <option value="loopback-connector-mysql">MySQL</option>
+                      <option value="loopback-connector-postgres">Postgres</option>
+                      <option value="loopback-connector-mongodb">Mongo DB</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="form-group">
-            <label for="database">Database</label>
-            <input id="database"
-              className="form-control"
-              name="database"
-              value={dsModel.database}
-              onChange={this.handleChange}
-              data-name="database"
-              type="text"
-              placeholder="database name" />
+          <div className="datasource-buttons-layout-container">
+            <button onClick={testConnection} data-id={dsModel.id}  className="model-detail-pocket-button model-save-button datasource-test-button">Test Connection</button>
           </div>
-          <div className="form-group">
-            <label for="host">Host</label>
-            <input id="host"
-              className="form-control"
-              name="host"
-              value={dsModel.host}
-              onChange={this.handleChange}
-              data-name="host"
-              type="text"
-              placeholder="host" />
-          </div>
-          <div className="form-group">
-            <label for="port">Port</label>
-            <input id="port"
-              className="form-control"
-              name="port"
-              value={dsModel.port}
-              onChange={this.handleChange}
-              data-name="port"
-              type="text"
-              placeholder="port" />
-          </div>
-          <div className="form-group">
-            <label for="userName">Username</label>
-            <input id="userName"
-              className="form-control"
-              name="userName"
-              type="text"
-              value={dsModel.userName}
-              onChange={this.handleChange}
-              data-name="userName"
-              placeholder="username" />
-          </div>
-          <div className="form-group">
-            <label for="password">Password</label>
-            <input id="password"
-              className="form-control"
-              name="password"
-              value={dsModel.password}
-              onChange={this.handleChange}
-              data-name="password"
-              type="password"
-              placeholder="password" />
-          </div>
-
-          <div>
-            <input type="button"
-              onClick={saveHandler}
-              className="primary pull-right"
-              value="save" />
-          </div>
-          <div>&nbsp;</div>
         </form>
       </div>
-
       );
   }
 });
