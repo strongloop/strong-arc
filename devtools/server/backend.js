edc854e6499b7310542289399355d7f66b51aaa1
Miroslav Bajtoš <miroslav@strongloop.com>
2014-09-03 13:49:33 +0200

    devtools: implement a simple protocol backend
    Implement a simple backend that responds with an empty response
    to most message.
    
    Override the default response with a more appropriate data for
    the few commands where the frontend requires a valid non-empty response.

--- devtools/server/backend.js
+++ devtools/server/backend.js
@@ -1,8 +1,40 @@
 var extend = require('util')._extend;
+var debug = require('debug')('studio:devtools');
+
 module.exports = DevToolsBackend;
 
 function DevToolsBackend() {
 }
+
+var sampleCpuProfile = loadExampleJsonSync('sample.cpuprofile');
+var sampleHeapSnapshot = loadExampleJsonSync('sample.heapsnapshot');
+
+var COMMANDS = {
+  'Worker.canInspectWorkers': { result: false },
+
+  'Page.getScriptExecutionStatus': { result: 'enabled' },
+  'Page.getResourceTree': {
+    frameTree: {
+      frame: {
+        id: 'dummy-toplevel-frame',
+        url: 'app.js',
+        securityOrigin: 'localhost'
+      },
+      resources: []
+    }
+  },
+
+  'IndexedDB.requestDatabaseNames': {
+    databaseNames: []
+  },
+
+  // Temporary implementation returning sample data
+  'Profiler.stop': { profile: sampleCpuProfile },
+
+  // TODO(bajtos) support Heap profiler
+  // 'HeapProfiler.takeHeapSnapshot'
+  // 'HeapProfiler.stopTrackingHeapObjects'
+};
 
 extend(DevToolsBackend.prototype, {
   serve: function(client) {
@@ -11,8 +43,36 @@ extend(DevToolsBackend.prototype, {
     this.client.on('error', this._onError.bind(this));
   },
 
-  _onRequest: function(message) {
-    console.log('TODO request', message);
+  _onRequest: function(data) {
+    var request;
+    try {
+      request = JSON.parse(data);
+    } catch(err) {
+      console.warn('**warn** malformed JSON request %j', data);
+    }
+    debug('request #%s %s', request.id, request.method, request.params);
+    this._handle(request);
+  },
+
+  _handle: function(request) {
+    var self = this;
+    var resultOrHandler = COMMANDS[request.method];
+    var fn = typeof resultOrHandler === 'function' ?
+      resultOrHandler :
+      function(params, callback) {
+        callback(null, resultOrHandler);
+      };
+
+    fn(request.params, function handleResult(err, result) {
+      debug('response #%s %s', request.id, request.method, result);
+      var response = { id: request.id };
+      if (err) {
+        response.error = err.toString();
+      } else {
+        response.result = result;
+      }
+      self._sendMessage(response);
+    });
   },
 
   _onError: function(err) {
@@ -23,3 +83,12 @@ extend(DevToolsBackend.prototype, {
     this.client.send(JSON.stringify(message));
   },
 });
+
+function loadExampleJsonSync(name) {
+  var fs = require('fs');
+  var path = require('path');
+  return JSON.parse(
+    fs.readFileSync(
+      path.resolve(__dirname, '..', 'examples', name),
+      'utf8'));
+}
