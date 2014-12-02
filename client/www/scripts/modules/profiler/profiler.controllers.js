fa4d65d9379097ecb85fac455e5a23b84eb7b2f5
seanbrookes <seanbrookes@beachair.ca>
2014-12-02 07:47:04 -0800

    fix resize issue hiding profiler header on iframe loading

--- client/www/scripts/modules/profiler/profiler.controllers.js
+++ client/www/scripts/modules/profiler/profiler.controllers.js
@@ -1,7 +1,16 @@
 Profiler.controller('ProfilerMainController',[
   '$scope',
-  '$location',
-  function($scope, $location){
+  '$log',
+  function($scope, $log){
+
+    // layout resizing to help control layout with devtools iframe
+    $scope.setProfilerLayout = function() {
+      var headerHeight = $('[data-id="AppHeaderContainer"]').outerHeight();
+      var profilerNavHeight = $('#ProfilerNavBar').outerHeight();
+      var windowHeight = $(window).outerHeight();
+      var devToolsHeight = (windowHeight - headerHeight - profilerNavHeight);
+      $('#ProfilerDevtoolsContainer').css('height', devToolsHeight);
+    }
   }
 ]);
 
