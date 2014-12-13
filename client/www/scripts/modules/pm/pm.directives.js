ab31768c1a8ee3d00b2e25eb187f5c106ca78aba
seanbrookes <seanbrookes@beachair.ca>
2014-12-12 18:11:20 -0800

    - add autoload to processes - add spinner to load process flow

--- client/www/scripts/modules/pm/pm.directives.js
+++ client/www/scripts/modules/pm/pm.directives.js
@@ -28,125 +28,141 @@ PM.directive('slPmHostForm', [
         '$timeout',
         'PMAppService',
         function($scope, $log, $timeout, PMAppService) {
-        $scope.currentServerConfig = {
-          host: '',
-          port: 0
-        };
-        $scope.pmServers = PMHostService.getPMServers();
-        $scope.candidateServerConfig = {};
-        // set value to last referenced server if available
-        if (PMHostService.getPMServers().length) {
-          $scope.candidateServerConfig = PMHostService.getLastPMServer();
-        }
-        $scope.selected = undefined;
-        $scope.activeProcess = null;
-        $scope.showMoreMenu = false;
-        $scope.isRemoteValid = false;
-        $scope.isOpen = false;
-        $scope.isLocalPMSelected = false;
-
-
-        $scope.processes = [];
-
-        $scope.onPMServerSelect = function(item) {
-          if (item.host === PM_CONST.LOCAL_PM_HOST_NAME) {
-            $scope.isLocalPMSelected = true;
+          $scope.currentServerConfig = {
+            host: '',
+            port: 0
+          };
+          $scope.pmServers = PMHostService.getPMServers();
+          $scope.candidateServerConfig = {};
+          // set value to last referenced server if available
+          if (PMHostService.getPMServers().length) {
+            $scope.candidateServerConfig = PMHostService.getLastPMServer();
           }
-          else {
-            $scope.isLocalPMSelected = false;
-          }
-          $scope.candidateServerConfig = item;
-        };
-        $scope.hideMenu = function(){
+          $scope.selected = undefined;
+          $scope.activeProcess = null;
+          $scope.showMoreMenu = false;
+          $scope.isRemoteValid = false;
           $scope.isOpen = false;
-        };
+          $scope.isLocalPMSelected = false;
 
-        function fireWhenReady(serverConfig, id) {
-          PMAppService.isLocalAppRunning()
-            .then(function(response) {
-              if (response.started === false) {
-                $timeout(function(){
-                  fireWhenReady(serverConfig, id);
-                }, 250);
-              }
-              else {
-                $scope.initServerProcesses(serverConfig, id);
-              }
-             });
-        }
 
-        $scope.loadProcesses = function(form){
-          if ( form.$valid ) {
-            $scope.processes = [];
-            $scope.currentServerConfig = $scope.candidateServerConfig;
+          $scope.processes = [];
 
-            if ($scope.currentServerConfig.host === PM_CONST.LOCAL_PM_HOST_NAME) {
-              // check if server is running or not
-              PMAppService.isLocalAppRunning()
-                .then(function(response) {
-                  if (response.started === false) {
-                    PMAppService.startLocalApp()
-                      .then(function(response) {
-                        // check to make sure the app comes up
-                        // probably need a poll
-                        // then load processes
-                        fireWhenReady($scope.currentServerConfig, 1);
-                      })
-                  }
-                  else {
-                    $scope.initServerProcesses($scope.currentServerConfig, 1);
-                  }
-                });
+          $scope.onPMServerSelect = function(item) {
+            if (item.host === PM_CONST.LOCAL_PM_HOST_NAME) {
+              $scope.isLocalPMSelected = true;
             }
             else {
-              $scope.initServerProcesses($scope.currentServerConfig, 1);
-
+              $scope.isLocalPMSelected = false;
             }
+            $scope.candidateServerConfig = item;
+          };
+          $scope.hideMenu = function(){
+            $scope.isOpen = false;
+          };
 
+          function fireWhenReady(serverConfig, id) {
+            PMAppService.isLocalAppRunning()
+              .then(function(response) {
+                if (response.started === false) {
+                  $timeout(function(){
+                    fireWhenReady(serverConfig, id);
+                  }, 250);
+                }
+                else {
+                  $scope.initServerProcesses(serverConfig, id);
+                }
+               });
           }
-        };
-        $scope.initServerProcesses = function(serverConfig, ServiceId) {
-          PMPidService.getDefaultPidData(serverConfig, ServiceId)
-            .then(function(pidCollection) {
-              PMHostService.addPMServer(serverConfig);
-              $scope.processes = pidCollection;
-              $scope.pmServers = PMHostService.getPMServers();
 
-              //activate first process
-              if ( $scope.processes.length ) {
-                $scope.setActiveProcess($scope.processes[0], false)
+          $scope.isShowPidDisplaySpinner = function() {
+            if ($scope.processes && $scope.processes.length > 0) {
+              return false;
+            }
+            return true;
+
+          };
+
+          $scope.submitPMHostForm = function(form) {
+            if ( form.$valid ) {
+              $scope.loadProcesses();
+            }
+          };
+
+          $scope.loadProcesses = function(){
+
+              $scope.processes = [];
+              $scope.currentServerConfig = $scope.candidateServerConfig;
+
+              if ($scope.currentServerConfig.host === PM_CONST.LOCAL_PM_HOST_NAME) {
+                // check if server is running or not
+                PMAppService.isLocalAppRunning()
+                  .then(function(response) {
+                    if (response.started === false) {
+                      PMAppService.startLocalApp()
+                        .then(function(response) {
+                          // check to make sure the app comes up
+                          // probably need a poll
+                          // then load processes
+                          fireWhenReady($scope.currentServerConfig, 1);
+                        })
+                    }
+                    else {
+                      $scope.initServerProcesses($scope.currentServerConfig, 1);
+                    }
+                  });
               }
-            });
-        };
+              else {
+                $scope.initServerProcesses($scope.currentServerConfig, 1);
 
-        //clear out active processes and remote state when going back to file
-        $scope.resetRemoteState = function(){
-          $scope.isRemoteValid = false;
-          $scope.processes = [];
-          $scope.activeProcess = null;
-
-        };
+              }
 
 
+          };
+          $scope.initServerProcesses = function(serverConfig, ServiceId) {
+            PMPidService.getDefaultPidData(serverConfig, ServiceId)
+              .then(function(pidCollection) {
+                PMHostService.addPMServer(serverConfig);
+                $scope.processes = pidCollection;
+                $scope.pmServers = PMHostService.getPMServers();
+
+                //activate first process
+                if ( $scope.processes.length ) {
+                  $scope.setActiveProcess($scope.processes[0], false)
+                }
+              });
+          };
+
+          //clear out active processes and remote state when going back to file
+          $scope.resetRemoteState = function(){
+            $scope.isRemoteValid = false;
+            $scope.processes = [];
+            $scope.activeProcess = null;
+
+          };
 
 
-        $scope.$watch('form.$valid', function(newVal, oldVal) {
-          if ( newVal !== oldVal && !newVal ) {
-            $scope.resetRemoteState();
-          }
-        });
-
-        $scope.setActiveProcess = function(process, isMoreClick){
-          if ( $scope.activeProcess && $scope.activeProcess.status !== 'Running' ) return false;
-
-          $scope.activeProcess = process;
-          $scope.isProcessFromMore = isMoreClick;
-          $log.debug('active process', process);
-          $scope.isRemoteValid = true;
 
 
-        };
-      }]
+          $scope.$watch('form.$valid', function(newVal, oldVal) {
+            if ( newVal !== oldVal && !newVal ) {
+              $scope.resetRemoteState();
+            }
+          });
+
+          $scope.setActiveProcess = function(process, isMoreClick){
+            if ( $scope.activeProcess && $scope.activeProcess.status !== 'Running' ) return false;
+
+            $scope.activeProcess = process;
+            $scope.isProcessFromMore = isMoreClick;
+            $log.debug('active process', process);
+            $scope.isRemoteValid = true;
+
+
+          };
+
+          $scope.loadProcesses();
+        }]
     }
   }
 ]);
