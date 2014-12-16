b1ab021eb81dbf638c971ff7216282021b4f2227
Ryan Graham <ryan@strongloop.com>
2014-12-15 23:04:52 -0800

    Add Number.isInteger polyfil
    Easy fix to get the Metrics view to work in Safari
    
    Helps with #840

--- client/www/scripts/sl.ui.js
+++ client/www/scripts/sl.ui.js
@@ -40,3 +40,14 @@ window.onresize = function(event) {
   window.isLocalStorageEnabled = false;
   return false;
 })();
+
+// ES6 Number.isInteger polyfil, needed for Safari and older IE.
+if (!Number.isInteger) {
+  Number.isInteger = function isInteger(nVal) {
+    return typeof nVal === 'number'
+    && isFinite(nVal)
+    && nVal > -9007199254740992
+    && nVal < 9007199254740992
+    && Math.floor(nVal) === nVal;
+  };
+}
