3c18dfd7cb94de28b0fa87d313750105e16116e4
Anthony Ettinger <anthony@strongloop.com>
2015-02-02 19:34:15 -0800

    remove extra events in SG controller

--- client/www/scripts/modules/styleguide/styleguide.controllers.js
+++ client/www/scripts/modules/styleguide/styleguide.controllers.js
@@ -5,14 +5,6 @@ Styleguide.controller('StyleguideController', [
   '$q',
   function ($scope, $log, StyleguideService, $q) {
     window.setScrollView('.common-instance-view-container');
-
-    $scope.delete = function(){
-      $log.log('delete clicked');
-    };
-
-    $scope.whatever = function(){
-      $log.log('whatever clicked');
-    };
   }
 ]);
 
