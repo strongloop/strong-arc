948b286498d1fbb5e95406c1ece37fda32fb25b9
seanbrookes <seanbrookes@beachair.ca>
2015-07-27 11:10:06 -0700

    initial add new gateway map modal/form upates

--- client/www/scripts/modules/gateway/gateway.map.directives.js
+++ client/www/scripts/modules/gateway/gateway.map.directives.js
@@ -5,10 +5,71 @@
  *
  * */
 Gateway.directive('slGatewayMapForm', [
-  function() {
+  'GatewayServices',
+  '$log',
+  'growl',
+  '$state',
+  function(GatewayServices,$log,growl,$state) {
     return {
       restrict: 'E',
-      templateUrl: './scripts/modules/gateway/templates/gateway.map.form.html'
+      scope: {
+        map: '=',
+        context: '=',
+        hidebuttons: '='
+      },
+      templateUrl: './scripts/modules/gateway/templates/gateway.map.form.html',
+      controller: ['$scope',
+        function($scope) {
+
+          $scope.isMapDirty = false;
+          $scope.originalMap = {};
+
+          function refreshMaps() {
+            context.gatewayMaps = GatewayServices.getGatewayMaps()
+              .then(function(maps) {
+
+                $scope.context.gatewayMaps = maps;
+
+              });
+          }
+
+          $scope.init = function() {
+            $scope.originalMap = angular.copy($scope.map);
+          };
+          $scope.init();
+
+          $scope.saveCurrentGatewayMap = function(map) {
+            if (map.name && map.endpoint) {
+
+              if ($scope.isMapDirty) {
+                if (confirm('do you want to make this change')) {
+                  GatewayServices.saveGatewayMap(map)
+                    .$promise
+                    .then(function(map) {
+                      growl.addSuccessMessage('Gateway Map Saved');
+                      //resetCurrentPolicy();
+
+                    });
+                }
+              }
+              else {
+                GatewayServices.saveGatewayMap(map)
+                  .$promise
+                  .then(function(map) {
+                    growl.addSuccessMessage('Gateway Map Saved');
+                    //resetCurrentPolicy();
+
+                  });
+              }
+
+
+
+
+
+            }
+          };
+        }
+      ]
     }
   }
 ]);
