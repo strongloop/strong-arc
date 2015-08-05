fbbfe73dd35ab54527232b88aa4a99096cebf197
seanbrookes <seanbrookes@beachair.ca>
2015-08-04 17:33:48 -0700

    mapping and policy functional cleanup
    - add check for existence of pipelines before opening new mapping modal
    - rearrange some broken CSS styles around policy form layouts
    - add icons to mapping list and policy list/detail
    - fix policy form layouts

--- client/www/scripts/modules/gateway/gateway.directives.js
+++ client/www/scripts/modules/gateway/gateway.directives.js
@@ -4,25 +4,35 @@
 Gateway.directive('slGatewayMainNav', [
   'GatewayServices',
   '$rootScope',
-  '$timeout',
+  '$http',
   '$log',
-  function(GatewayServices, $rootScope, $timeout, $log) {
+  function(GatewayServices, $rootScope, $http, $log) {
     return {
       replace: true,
       link: function(scope,  el, attrs) {
 
+        scope.svgIcons = [];
+
+        $http({
+          method: 'GET',
+          url: './scripts/modules/ui/templates/ui.icons.svg.html'
+        })
+        .success(function(data) {
+            scope.svgIcons = data;
+          })
+        .error(function(error) {
+            $log.warn('bad get icon svg files: ' + JSON.stringify(error));
+          });
+
         scope.$watch('gatewayMapCtx.gatewayMaps', function(newVal, oldVal) {
           if (newVal && newVal.map) {
 
-            $log.debug('scope.$apply 1: ' + scope.$apply);
               React.renderComponent(GatewayMainNav({scope:scope, rootScope:$rootScope}), el[0]);
 
           }
         }, true);
         scope.$watch('pipelineCtx.pipelines', function(newVal, oldVal) {
           if (newVal && newVal.map) {
-            $log.debug('scope.$apply 2: ' + scope.$apply);
-
               React.renderComponent(GatewayMainNav({scope:scope, rootScope:$rootScope}), el[0]);
 
           }
@@ -30,8 +40,6 @@ Gateway.directive('slGatewayMainNav', [
 
         scope.$watch('policyCtx.policies', function(newVal, oldVal) {
           if (newVal && newVal.map) {
-            $log.debug('scope.$apply 3: ' + scope.$apply);
-
               React.renderComponent(GatewayMainNav({scope:scope, rootScope:$rootScope}), el[0]);
 
           }
