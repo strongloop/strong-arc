82279574bc27a7f8a9c59efff58b3ac9db58420e
seanbrookes <seanbrookes@beachair.ca>
2015-08-07 17:47:53 -0700

    initial gateway e2e tests

--- client/test/e2e/arc/tmp.gateway.spec.js
+++ client/test/e2e/arc/tmp.gateway.spec.js
@@ -22,6 +22,112 @@ describe('gateway smoke test', function() {
 
         landingView.openGatewayView();
         expect(EC.visibilityOf(gatewayHomeView.componentIdentifier));
+        expect(EC.visibilityOf(gatewayHomeView.policyListViewButton));
+        gatewayHomeView.policyListViewButton.click();
+        browser.sleep(700);
+        expect(EC.visibilityOf(gatewayHomeView.policyContainer));
+        // Policy
+        expect(EC.visibilityOf(gatewayHomeView.sideNewPolicyButton));
+        gatewayHomeView.sideNewPolicyButton.click();
+        browser.sleep(500);
+        expect(EC.visibilityOf(gatewayHomeView.closeModalButton));
+        gatewayHomeView.closeModalButton.click();
+        browser.sleep(1000);
+        expect(EC.visibilityOf(gatewayHomeView.mainNewPolicyButton));
+        gatewayHomeView.mainNewPolicyButton.click();
+        browser.sleep(500);
+        expect(EC.visibilityOf(gatewayHomeView.closeModalButton));
+        gatewayHomeView.closeModalButton.click();
+        browser.sleep(1000);
+
+
+
+        // ok now create a policy
+        gatewayHomeView.sideNewPolicyButton.click();
+        browser.sleep(500);
+        expect(EC.visibilityOf(gatewayHomeView.newPolicyNameInput));
+        expect(EC.visibilityOf(gatewayHomeView.newPolicyTypeSelect));
+        browser.sleep(2000);
+        gatewayHomeView.newPolicyNameInput.sendKeys('new policy name');
+        browser.sleep(2000);
+        gatewayHomeView.newPolicyTypeSelect.click();
+        browser.sleep(1000);
+        //gatewayHomeView.newPolicyTypeSelect.click();
+        //browser.sleep(1000);
+       // browser.sleep(1000);
+        expect(EC.visibilityOf(gatewayHomeView.policyTypeMetricsSelect));
+
+        gatewayHomeView.policyTypeMetricsSelect.click();
+        browser.sleep(1000);
+
+        expect(gatewayHomeView.newPolicyTypeSelect.getText()).toEqual('metrics');
+
+        browser.sleep(1000);
+        expect(EC.visibilityOf(gatewayHomeView.saveNewInstanceButton));
+
+        gatewayHomeView.saveNewInstanceButton.click();
+
+
+        //gatewayHomeView.closeModalButton.click();
+        browser.sleep(700);
+
+
+        expect(EC.visibilityOf(gatewayHomeView.editPolicyNameInput));
+        expect(gatewayHomeView.editPolicyNameInput.getAttribute('value')).toEqual('new policy name');
+
+        browser.sleep(700);
+        gatewayHomeView.policyListViewButton.click();
+        browser.sleep(700);
+        //
+        //expect(EC.visibilityOf(gatewayHomeView.deletePolicyButton));
+        //gatewayHomeView.deletePolicyButton.click();
+
+
+        // deal with dialog
+
+
+        // pipeline
+        gatewayHomeView.pipelineListViewButton.click();
+        browser.sleep(700);
+        expect(EC.visibilityOf(gatewayHomeView.pipelineContainer));
+        expect(EC.visibilityOf(gatewayHomeView.sideNewPipelineButton));
+        gatewayHomeView.sideNewPipelineButton.click();
+        browser.sleep(500);
+        expect(EC.visibilityOf(gatewayHomeView.closeModalButton));
+        gatewayHomeView.closeModalButton.click();
+        browser.sleep(1000);
+        expect(EC.visibilityOf(gatewayHomeView.mainNewPipelineButton));
+        gatewayHomeView.mainNewPipelineButton.click();
+        browser.sleep(500);
+        expect(EC.visibilityOf(gatewayHomeView.closeModalButton));
+        gatewayHomeView.closeModalButton.click();
+        browser.sleep(1000);
+
+
+
+
+        // mapping
+        gatewayHomeView.gatewaymapListViewButton.click();
+        browser.sleep(700);
+        expect(EC.visibilityOf(gatewayHomeView.mappingContainer));
+        expect(EC.visibilityOf(gatewayHomeView.sideNewMappingButton));
+        //gatewayHomeView.sideNewMappingButton.click();
+        //browser.sleep(500);
+        //expect(EC.visibilityOf(gatewayHomeView.closeModalButton));
+        //gatewayHomeView.closeModalButton.click();
+        //browser.sleep(1000);
+        //expect(EC.visibilityOf(gatewayHomeView.mainNewMappingButton));
+        //gatewayHomeView.mainNewMappingButton.click();
+        //browser.sleep(500);
+        //expect(EC.visibilityOf(gatewayHomeView.closeModalButton));
+        //gatewayHomeView.closeModalButton.click();
+        //browser.sleep(1000);
+
+
+
+
+
+        //browser.pause();
         headerView.logout();
       });
   });
--- client/test/e2e/gateway/views/gateway-home-view.js
+++ client/test/e2e/gateway/views/gateway-home-view.js
@@ -6,6 +6,43 @@ var GatewayHomeView = (function () {
     self.componentIdentifier = element(
       by.css('span.ia-project-title-container'));
 
+
+    // policy
+    self.policyListViewButton = element(by.css('button[data-type="policy"].tree-branch'));
+    self.policyContainer = element(by.css('div[ng-controller="PolicyMainController"]'));
+
+    self.editPolicyNameInput = element(by.css('input[type="text"][ng-model="policy.name"]'));
+    self.newPolicyNameInput = element(by.css('div.modal-body input[type="text"][ng-model="policy.name"]'));
+    self.newPolicyTypeSelect = element(by.css('div.modal-body div.policy-type-select-container button.toggle-btn'));
+    self.deletePolicyButton = element(by.css('table.policies button[ng-click="deletePolicy(policy)"]'));
+    //self.newPolicyTypeValueAttrib = element(by.css('div.modal-body div.policy-type-select-container button.toggle-btn'));
+    self.policyTypeAuthSelect = element(by.css('div.modal-body div.policy-type-select-container ul.dropdown-menu li button[value="auth"]'));
+    self.policyTypeMetricsSelect = element(by.css('div.modal-body div.policy-type-select-container ul.dropdown-menu li button[value="metrics"]'));
+    self.policyTypeRateLimitingSelect = element(by.css('div.modal-body div.policy-type-select-container ul.dropdown-menu li button[value="reverseproxy"]'));
+    self.policyTypeReversProxySelect = element(by.css('div.modal-body div.policy-type-select-container ul.dropdown-menu li button[value="gatewaymap"]'));
+
+    self.closeModalButton = element(by.css('button[ng-click="close()"]'));
+    self.saveNewInstanceButton = element(by.css('div.modal-footer button.primary'));
+
+
+    self.pipelineListViewButton = element(by.css('button[data-type="pipeline"].tree-branch'));
+    self.pipelineContainer = element(by.css('div[ng-controller="PipelineMainController"]'));
+
+    self.sideNewPolicyButton = element(by.css('button[data-type="policy"].nav-tree-item-addnew'));
+    self.mainNewPolicyButton = element(by.css('div.entity-list-container button[data-type="policy"].add-new'));
+
+    self.sideNewPipelineButton = element(by.css('button[data-type="pipeline"].nav-tree-item-addnew'));
+    self.mainNewPipelineButton = element(by.css('div.entity-list-container button[data-type="pipeline"].add-new'));
+
+    self.sideNewMappingButton = element(by.css('button[data-type="gatewaymap"].nav-tree-item-addnew'));
+    self.mainNewMappingButton = element(by.css('div.entity-list-container button[data-type="gatewaymap"].add-new'));
+
+
+
+    self.gatewaymapListViewButton = element(by.css('button[data-type="gatewaymap"].tree-branch'));
+
+    self.mappingContainer = element(by.css('div[ng-controller="GatewayMapMainController"]'));
+    //GatewayMapMainController
     self.waitUntilLoaded = function() {
       browser.driver.wait(
         EC.presenceOf(self.componentIdentifier),
